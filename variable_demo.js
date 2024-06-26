8625fe97e4ed34bc3585ae7de39f443c4f28df50
Hema Nalawade <98449213+HemaNalawade@users.noreply.github.com>
2024-06-26 17:41:22 +0530

    Add files via upload

---
+++ variable_demo.js
@@ -0,0 +1,17 @@
+//declaring variable in javascript
+var a;
+console.log(a)
+a=10
+console.log(a)//a is of number type
+a=true
+console.log(a)//datatype of a is boolean
+a="hello"
+console.log(a)// datatype of a string
+
+var rollNo=12, name="hema", gender="female"
+console.log("roll no:",rollNo)
+console.log("name:"+name + " " +"Gender:"+gender)
+
+var x=60, y=45, z
+z=x+y
+console.log("addition is",z)
